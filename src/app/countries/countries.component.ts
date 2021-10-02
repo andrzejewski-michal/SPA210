14ea70e06f29dd9fe8d53949f76f198ef93689d9
Michał Andrzejewski <mandrzejewski@MacBook-Pro-Micha.local>
2021-10-02 18:53:06 +0200

    app

--- src/app/countries/countries.component.ts
+++ src/app/countries/countries.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Countries } from '../myclasses/countries';
 
 @Component({
   selector: 'app-countries',
@@ -6,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./countries.component.css']
 })
 export class CountriesComponent implements OnInit {
+
+myCountries="My Favorite Countries"
+
+countries_tab = [
+  new Countries('Poland', 312679),
+  new Countries('Denmark', 421321),
+  new Countries('Germany', 6541223),
+  new Countries('Russia', 63244253),
+  new Countries('Ukraine', 532153),
+]
 
   constructor() { }
 
